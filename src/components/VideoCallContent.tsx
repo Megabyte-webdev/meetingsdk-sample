16acb86bfa7b974cdae2555067f0d4b1e3c9dc75
Megabyte-webdev <afolabimubarak18@gmail.com>
2026-07-23 18:34:29 +0100

    update: bump @afosecure/meetingsdk and uuid versions; change server URL to local API

--- src/components/VideoCallContent.tsx
+++ src/components/VideoCallContent.tsx
@@ -20,7 +20,7 @@ import IncallChat from "./IncallChat";
 import ParticipantPanel from "./ParticipantPanel";
 
 const USER_ID = "37d6faf5-718f-4766-a9ab-aa1a4a05005a";
-const SERVER = "https://rust-video-server-sfyf.onrender.com";
+const SERVER = "http://localhost:8080/api";
 
 function VideoCallContent() {
   const {
